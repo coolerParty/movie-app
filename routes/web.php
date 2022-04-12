78c02c9d99426cac588e4278896608f540299e76
coolerParty <alexmeiers2014@gmail.com>
2022-04-12 16:53:21 +0800

    Roles and Permissions Added

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified','role:admin'])->get('/admin', function () {
+    return "admin ";
+})->name('admin.index');
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
