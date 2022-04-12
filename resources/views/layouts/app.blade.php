0ae53ed30b847fa7852f442e135912938bb2b479
coolerParty <alexmeiers2014@gmail.com>
2022-04-12 13:09:26 +0800

    Change Dashboard, added sidebar and header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+	<title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+	<!-- Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        @livewireStyles
+	<!-- Styles -->
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+	<!-- Scripts -->
+	<script src="{{ mix('js/app.js') }}" defer></script>
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+	@livewireStyles
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+<body class="font-sans antialiased">
+	<x-jet-banner />
 
-        @stack('modals')
+	<div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
+		<x-sidebar></x-sidebar>
+		<div class="flex-1 flex flex-col overflow-hidden">
+			<x-header></x-header>
+			<!-- Page Heading -->
+			@if (isset($header))
+				<header class="bg-white shadow">
+					<div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+						{{ $header }}
+					</div>
+				</header>
+			@endif
 
-        @livewireScripts
-    </body>
+			<!-- Page Content -->
+			<main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+				<div class="container mx-auto px-6 py-8">
+
+					{{ $slot }}
+
+				</div>
+			</main>
+		</div>
+	</div>
+
+	@stack('modals')
+
+	@livewireScripts
+</body>
+
 </html>
