f4a09097e507a4dc343ff68851d71dd487708ae9
coolerParty <alexmeiers2014@gmail.com>
2022-04-14 09:07:32 +0800

    Create Tag Added

--- app/Http/Livewire/TagIndex.php
+++ app/Http/Livewire/TagIndex.php
@@ -1,11 +1,35 @@
 <?php
 
 namespace App\Http\Livewire;
+use App\Models\Tag;
+use Illuminate\Support\Str;
 
 use Livewire\Component;
 
 class TagIndex extends Component
 {
+    public $showTagModal = false;
+    public $tagName;
+
+    public function showCreateModal()
+    {
+        $this->showTagModal = true;
+    }
+
+    public function createTag()
+    {
+        Tag::create([
+            'tag_name' => $this->tagName,
+            'slug'     => Str::slug($this->tagName),
+        ]);
+        $this->reset();
+    }
+
+    public function closeTagModal()
+    {
+        $this->showTagModal = false;
+    }
+
     public function render()
     {
         return view('livewire.tag-index');
--- resources/views/livewire/tag-index.blade.php
+++ resources/views/livewire/tag-index.blade.php
@@ -1,7 +1,7 @@
 <section class="container mx-auto p-6 font-mono">
-    <div class="w-full flex mb-4 p-2 justify-end">
-        <x-jet-button>Create Tag</x-jet-button>
-    </div>
+	<div class="w-full flex mb-4 p-2 justify-end">
+		<x-jet-button wire:click="showCreateModal">Create Tag</x-jet-button>
+	</div>
 	<div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
 		<div class="w-full overflow-x-auto">
 			<table class="w-full">
@@ -26,12 +26,42 @@
 						</td>
 						<td class="px-4 py-3 text-sm border">Public</td>
 						<td class="px-4 py-3 text-sm border">
-                            Edit/Delete
-                        </td>
+							Edit/Delete
+						</td>
 					</tr>
-					
+
 				</tbody>
 			</table>
 		</div>
 	</div>
+	<x-jet-dialog-modal wire:model="showTagModal">
+		<x-slot name="title">Create Tag</x-slot>
+		<x-slot name="content">
+
+			<div class="mt-10 sm:mt-0">
+				<div class="mt-5 md:mt-0 md:col-span-2">
+					<form>
+						<div class="shadow overflow-hidden sm:rounded-md">
+							<div class="px-4 py-5 bg-white sm:p-6">
+								<div class="grid grid-cols-6 gap-6">
+									<div class="col-span-6 sm:col-span-3">
+										<label for="first-name" class="block text-sm font-medium text-gray-700">Tag name</label>
+										<input wire:model="tagName" type="text" autocomplete="given-name"
+											class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</form>
+				</div>
+			</div>
+
+		</x-slot>
+		<x-slot name="footer">
+			<x-jet-button wire:click="closeTagModal">Cancel</x-jet-button>
+			<button wire:click="createTag"
+				class="ml-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create</button>
+		</x-slot>
+	</x-jet-dialog-modal>
 </section>
