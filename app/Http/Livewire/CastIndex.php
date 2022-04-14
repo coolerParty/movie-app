8c412797c502abcebed8dfc86d515244b52a74bd
coolerParty <alexmeiers2014@gmail.com>
2022-04-14 19:03:54 +0800

    Create Casts Added

--- app/Http/Livewire/CastIndex.php
+++ app/Http/Livewire/CastIndex.php
@@ -3,11 +3,39 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Cast;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\HTTP;
+use Illuminate\Support\Str;
 
 class CastIndex extends Component
 {
+    use WithPagination;
+
+    protected $key = '5267a519dbe54ffbef5e4a2ede3f35b0';
+
+    public $castTMDBId;
+    public $castName;
+    public $castPosterPath;
+
+    public function generateCast()
+    {
+        $newCast = HTTP::get('https://api.themoviedb.org/3/person/10859?api_key=5267a519dbe54ffbef5e4a2ede3f35b0')
+        ->json();
+
+        Cast::create([
+            'tmdb_id'     => $newCast['id'],
+            'name'        => $newCast['name'],
+            'slug'        => Str::slug($newCast['name']),
+            'poster_path' => $newCast['profile_path'],
+        ]);
+        
+    }
+
     public function render()
     {
-        return view('livewire.cast-index');
+        return view('livewire.cast-index',[
+            'casts' => Cast::paginate(5)
+        ]);
     }
 }
--- resources/views/livewire/cast-index.blade.php
+++ resources/views/livewire/cast-index.blade.php
@@ -1,37 +1,53 @@
 <section class="container mx-auto p-6 font-mono">
-   <div class="w-full flex mb-4 p-2 justify-end">
-       <x-jet-button>Create Cast</x-jet-button>
-   </div>
-  <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-     <div class="w-full overflow-x-auto">
-        <table class="w-full">
-           <thead>
-              <tr
-                 class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                 <th class="px-4 py-3">Title</th>
-                 <th class="px-4 py-3">Date</th>
-                 <th class="px-4 py-3">Rating</th>
-                 <th class="px-4 py-3">Public</th>
-                 <th class="px-4 py-3">Manage</th>
-              </tr>
-           </thead>
-           <tbody class="bg-white">
-              <tr class="text-gray-700">
-                 <td class="px-4 py-3 border">
-                    Title Here
-                 </td>
-                 <td class="px-4 py-3 text-ms font-semibold border">Date Here</td>
-                 <td class="px-4 py-3 text-xs border">
-                    Rating Here
-                 </td>
-                 <td class="px-4 py-3 text-sm border">Public</td>
-                 <td class="px-4 py-3 text-sm border">
-                           Edit/Delete
-                       </td>
-              </tr>
-              
-           </tbody>
-        </table>
-     </div>
-  </div>
+	<div class="w-full flex mb-4 p-2 justify-end">
+		<form>
+			<div class="shadow overflow-hidden sm:rounded-md">
+				<div class="px-4 py-5 bg-white sm:p-6">
+					<div class="grid grid-cols-6 gap-6">
+						<div class="col-span-6 sm:col-span-3">
+							<label for="first-name" class="block text-sm font-medium text-gray-700">Cast TBMD ID</label>
+							<input wire:model="castTMDBId" type="text" autocomplete="given-name"
+								class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</form>
+		<x-jet-button wire:click="generateCast" >Generate Cast</x-jet-button>
+	</div>
+	<div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+		<div class="w-full overflow-x-auto">
+			<table class="w-full">
+				<thead>
+					<tr
+						class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+						<th class="px-4 py-3">Name</th>
+						<th class="px-4 py-3">Slug</th>
+						<th class="px-4 py-3">Poster</th>
+						<th class="px-4 py-3">Manage</th>
+					</tr>
+				</thead>
+				<tbody class="bg-white">
+					@foreach ($casts as $cast)
+						<tr class="text-gray-700">
+							<td class="px-4 py-3 border">
+								{{ $cast->name }}
+							</td>
+							<td class="px-4 py-3 text-ms font-semibold border">
+								{{ $cast->slug }}
+							</td>
+							<td class="px-4 py-3 text-xs border">
+								{{ $cast->poster_path }}
+							</td>
+							<td class="px-4 py-3 text-sm border">
+								Edit/Delete
+							</td>
+						</tr>
+					@endforeach
+				</tbody>
+				{{ $casts->links() }}
+			</table>
+		</div>
+	</div>
 </section>
