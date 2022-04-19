3f73d45445fd9e0f05b00f9e99be54003c82d666
coolerParty <alexmeiers2014@gmail.com>
2022-04-19 22:08:36 +0800

    Start of Movie

--- app/Http/Livewire/MovieIndex.php
+++ app/Http/Livewire/MovieIndex.php
@@ -3,11 +3,26 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Movie;
+use Livewire\WithPagination;
 
 class MovieIndex extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+    public $sort = 'asc';
+    public $perPage = '5';
+
+    public function resetFilters()
+    {
+        $this->reset(['search','sort','perPage']);
+    }
+
     public function render()
     {
-        return view('livewire.movie-index');
+        return view('livewire.movie-index',[
+            'movies' => Movie::search('title', $this->search)->orderBy('title', $this->sort)->paginate($this->perPage),
+        ]);
     }
 }
